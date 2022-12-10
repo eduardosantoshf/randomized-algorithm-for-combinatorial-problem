203443316bde4d21e7060bf6141848e1f8153d4d
Eduardo Santos <eduardosantoshf@gmail.com>
2022-12-10 14:23:20 +0000

    Final version of the plot results script and some results

--- plot_results.py
+++ plot_results.py
@@ -1,4 +1,5 @@
 from graph import Graph
+import matplotlib as mpl
 import matplotlib.pyplot as plot
 from matplotlib.pyplot import figure
 import json
@@ -13,7 +14,7 @@ def compute_results(maximum_nodes_number: int, percentage: float):
         g3 = Graph().random_graph(n, 93107, edge_probability = 0.25)
 
         minimum_weighted_closure1, iterations1, execution_time1, solutions_number1 = \
-            g1.find_minimum_weighted_closure(93107, 1, 1)
+            g1.find_minimum_weighted_closure(93107, 1, 1, 0.25)
         minimum_weighted_closure2, iterations2, execution_time2, solutions_number2 = \
             g2.find_minimum_weighted_closure(93107, percentage, 1, 0.25)
         minimum_weighted_closure3, iterations3, execution_time3, solutions_number3 = \
@@ -101,7 +102,59 @@ def plot_results(comparison: str, percentage: float):
     axs[2].grid(linestyle = "--",linewidth = 0.2)
 
     plot.show()
-        
+
+def compute_benchmark_graphs_results(percentage: float):
+    data = dict()
+
+    g = Graph().read_graph("graphs/SWtinyEWD.txt", 93107)
+
+    minimum_weighted_closure1, iterations1, execution_time1, solutions_number1 = \
+            g.find_minimum_weighted_closure(93107, 1, 1, 0.25)
+    minimum_weighted_closure2, iterations2, execution_time2, solutions_number2 = \
+            g.find_minimum_weighted_closure(93107, percentage, 1, 0.25)
+    minimum_weighted_closure3, iterations3, execution_time3, solutions_number3 = \
+            g.find_minimum_weighted_closure(93107, 1, percentage, 0.25)
+
+    data["benchmark_exhaustive"] = [iterations1, solutions_number1, minimum_weighted_closure1, execution_time1]
+    data["benchmark__s_" + str(percentage)] = [iterations2, solutions_number2, minimum_weighted_closure2, execution_time2]
+    data["benchmark__t_" + str(percentage)] = [iterations3, solutions_number3, minimum_weighted_closure3, execution_time3]
+
+    with open("results/benchmark_graph.txt", 'w') as file:
+        file.write(json.dumps(data))
+
+def plot_benchmark_graphs_results():
+    data = dict()
+
+    with open("results/benchmark_graph.txt", 'r') as file:
+        data = json.load(file)
+
+    iterations, solutions_number, execution_time = dict(), dict(), dict()
+
+    for key in data.keys():
+        iterations[key] = data[key][0]
+        solutions_number[key] = data[key][1]
+        execution_time[key] = data[key][3]
+
+    mpl.rc('axes', labelcolor='b')
+
+    fig, axs = plot.subplots(3, figsize=(7, 6))
+
+    axs[0].set_xlabel('algorithm')
+    axs[0].set_ylabel('iterations') 
+    axs[0].bar(iterations.keys(), iterations.values(), width = 0.3)
+
+    axs[1].set_xlabel('algorithm')
+    axs[1].set_ylabel('solutions number') 
+    axs[1].bar(solutions_number.keys(), solutions_number.values(), width = 0.3)
+
+    axs[2].set_xlabel('algorithm')
+    axs[2].set_ylabel('execution time')
+    axs[2].bar(execution_time.keys(), execution_time.values(), width = 0.3)
+
+    fig.tight_layout(pad = 3)
+
+    plot.show()
+
 
 if __name__ == '__main__':
     maximum_nodes_number = 25
@@ -112,3 +165,6 @@ if __name__ == '__main__':
     #plot_results("execution_time", percentage)
     #plot_results("iterations", percentage)
     #plot_results("solutions_number", percentage)
+
+    #compute_benchmark_graphs_results(percentage)
+    plot_benchmark_graphs_results()
